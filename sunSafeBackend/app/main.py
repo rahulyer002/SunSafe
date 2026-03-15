130f14c7eae2b6f17f764d5f94bbee4564b4a8b6
rahuly-adpmn <hanumantharn@adpmn.com>
2026-03-15 21:06:25 +1100

    allowing cors error from backend

--- sunSafeBackend/app/main.py
+++ sunSafeBackend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers.uv_router import router as uv_router
 from app.routers.skin_router import router as skin_router
@@ -9,6 +10,18 @@ app = FastAPI(
     version="1.0"
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(uv_router)
 app.include_router(skin_router)
 app.include_router(cancer_router)
+
+@app.get("/")
+def root():
+    return {"message": "SunSafe API running"}
